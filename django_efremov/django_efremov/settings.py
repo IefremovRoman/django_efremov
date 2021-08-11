22569c0ea71a633d9ebf2de78ffd68ed38a9d41e
IefremovRoman <iefremov.roman@gmail.com>
2021-08-11 14:04:31 +0300

    add setting.py

--- django_efremov/django_efremov/settings.py
+++ django_efremov/django_efremov/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'students.middleware.simple_middleware',
 ]
 
 ROOT_URLCONF = 'django_efremov.urls'
