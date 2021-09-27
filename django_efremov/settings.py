e6f68c98355a5f55c6f914023f6eb56ff28fb5ed
IefremovRoman <iefremov.roman@gmail.com>
2021-09-27 17:34:05 +0300

    Groups' class-based views get done

--- django_efremov/settings.py
+++ django_efremov/settings.py
@@ -50,7 +50,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
