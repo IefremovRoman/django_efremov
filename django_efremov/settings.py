fbc8e23399c019a2b881e01a9e847e96a3b6d003
IefremovRoman <iefremov.roman@gmail.com>
2021-10-18 19:16:43 +0300

    Ready to deploy

--- django_efremov/settings.py
+++ django_efremov/settings.py
@@ -188,7 +188,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'django_efremov', 'static')]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
